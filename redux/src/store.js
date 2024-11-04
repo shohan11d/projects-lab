4711306345e723d28a7c67997bc4b57392464bbf
shohan11d <shohan11d@gmail.com>
2024-11-05 02:18:22 +0600

    created redux store

---
+++ redux/src/store.js
@@ -0,0 +1,42 @@
+import { createStore } from 'redux';
+const initialState = {
+   balance: 0,
+   loan: 0,
+   loanPurpose: '',
+};
+
+function reducer(state = initialState, action) {
+   switch (action.type) {
+      case 'account/deposit':
+         return { ...state, balance: state.balance + action.payload };
+      case 'account/withdraw':
+         return { ...state, balance: state.balance + action.payload };
+      case 'account/requestLoan':
+         if (state.loan > 0) return state;
+         return {
+            ...state,
+            loan: action.payload.amount,
+            balance: state.balance + action.payload.amount,
+            loanPurpose: action.payload.purpose,
+         };
+      case 'account/payLoan':
+         return {
+            ...state,
+            loan: 0,
+            loanPurpose: '',
+            balance: state.balance - state.loan,
+         };
+      default:
+         return state;
+   }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+store.dispatch({
+   type: 'account/requestLoan',
+   payload: { amount: 200, purpose: 'Buy a car' },
+});
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());

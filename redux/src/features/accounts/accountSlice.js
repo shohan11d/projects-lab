7ddd0e10ab0309fd293cb2481e1eb9be6f1b7cc8
shohan11d <shohan11d@gmail.com>
2024-11-06 15:22:15 +0600

    rtk converted

--- redux/src/features/accounts/accountSlice.js
+++ redux/src/features/accounts/accountSlice.js
@@ -1,10 +1,13 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
    balance: 0,
    loan: 0,
    loanPurpose: '',
    isLoading: false,
 };
 
+/*
 export default function accountReducer(state = initialStateAccount, action) {
    switch (action.type) {
       case 'account/deposit':
@@ -64,3 +67,36 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
    return { type: 'account/payLoan' };
 }
+*/
+const accountSlice = createSlice({
+   name: 'account',
+   initialState,
+   reducers: {
+      deposit: (state, action) => {
+         state.balance += action.payload;
+      },
+      withdraw: (state, action) => {
+         state.balance -= action.payload;
+      },
+      requestLoan: {
+         prepare: (amount, purpose) => {
+            return { payload: { amount, purpose } };
+         },
+         reducer(state, action) {
+            if (state.loan > 0) return;
+            state.loan = action.payload.amount;
+            state.loanPurpose = action.payload.purpose;
+            state.balance = state.balance + action.payload.amount;
+         },
+      },
+
+      payLoan: (state) => {
+         state.balance = state.balance - state.loan;
+         state.loan = 0;
+         state.loanPurpose = '';
+      },
+   },
+});
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export default accountSlice.reducer;
+console.log(accountSlice);

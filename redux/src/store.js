893732f640accda43360f9d6151d9e3540d5f124
shohan11d <shohan11d@gmail.com>
2024-11-06 13:08:15 +0600

    redux classic done

--- redux/src/store.js
+++ redux/src/store.js
@@ -1,10 +1,15 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const rootReducer = combineReducers({
    account: accountReducer,
    customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(
+   rootReducer,
+   composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
